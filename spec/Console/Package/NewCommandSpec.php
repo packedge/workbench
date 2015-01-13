ecfe5bd185f8fcb45652c2fcfdd5ca62f8b2a3a6
Ashley Clarke <me@ashleyclarke.me>
2015-01-14 00:29:21 +0100

    update tests

--- spec/Console/Package/NewCommandSpec.php
+++ spec/Console/Package/NewCommandSpec.php
@@ -2,11 +2,18 @@
 
 namespace spec\Packedge\Workbench\Console\Package;
 
+use Illuminate\Filesystem\Filesystem;
+use Packedge\Workbench\Package;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class NewCommandSpec extends ObjectBehavior
 {
+    public function let(Filesystem $filesystem, Package $package)
+    {
+        $this->beConstructedWith($filesystem, $package);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Packedge\Workbench\Console\Package\NewCommand');
--- spec/Parsers/PackageParserSpec.php
+++ spec/Parsers/PackageParserSpec.php
@@ -16,11 +16,21 @@ class PackageParserSpec extends ObjectBehavior
     public function it_parses_a_package_name_into_a_directory_name()
     {
         $this->parse('example/project')->toDirectoryName()->shouldReturn('example-project');
+        $this->parse('example-vendor/project')->toDirectoryName()->shouldReturn('example-vendor-project');
+        $this->parse('example/project-name')->toDirectoryName()->shouldReturn('example-project-name');
+        $this->parse('example-vendor/project-name')->toDirectoryName()->shouldReturn('example-vendor-project-name');
     }
 
     /** @test */
     public function it_parses_a_package_name_into_a_psr4_namespace()
     {
         $this->parse('example/project')->toPsr4()->shouldReturn('Example\\\\Project');
+    }
+
+    /** @test */
+    public function it_parses_a_package_name_into_a_human_readable_name()
+    {
+        $this->parse('example/project')->toHuman()->shouldReturn('Project');
+        $this->parse('example/project-name')->toHuman()->shouldReturn('Project Name');
     }
 }
