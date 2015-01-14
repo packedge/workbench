288e0765b3f15a31f0eba2bdf924437e91eab866
Ashley Clarke <me@ashleyclarke.me>
2015-01-14 20:17:04 +0100

    fix test

--- spec/Console/Package/NewCommandSpec.php
+++ spec/Console/Package/NewCommandSpec.php
@@ -2,11 +2,17 @@
 
 namespace spec\Packedge\Workbench\Console\Package;
 
+use Packedge\Workbench\Package;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class NewCommandSpec extends ObjectBehavior
 {
+    public function let(Package $package)
+    {
+        $this->beConstructedWith($package);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Packedge\Workbench\Console\Package\NewCommand');
